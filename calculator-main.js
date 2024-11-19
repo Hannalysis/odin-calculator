615c6c41184928b5d35a2cf95cc2846fe4dcdf5a
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2024-11-19 15:44:16 +0000

    Operate function useable with switch case loop
    Utilising divide & multiply unicode symbols for use in html calculator.

--- calculator-main.js
+++ calculator-main.js
@@ -1,3 +1,8 @@
+// Unicode characters for visual mathematical operators
+
+const divideSymbol = '\u00F7';
+const multiplySymbol = '\u00D7';
+
 // Calculator Functions
 
 const add = function(num1, num2) {
@@ -16,8 +21,26 @@ const divide = function(num1, num2) {
   return num1 / num2;
 };
 
-operations = {'+' : add, '-' : subtract, '*' : multiply, "/" : divide}
+// operations = {'+' : add, '-' : subtract, '*' : multiply, "/" : divide}
 
 const operate = function(num1, operator, num2){
-    // console.log(operations[operator]);
+  switch(operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case multiplySymbol:
+      return  multiply(num1, num2);
+    case divideSymbol:
+      return divide(num1, num2);
+  }
 }
+
+//Test code
+console.log(divideSymbol);
+console.log(multiplySymbol);
+
+console.log(operate(10, '+', 11));
+console.log(operate(5, '-', 2));
+console.log(operate(10, multiplySymbol, 11));
+console.log(operate(10, divideSymbol, 2));
